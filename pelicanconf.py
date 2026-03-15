5ea5e8dfd723631c8cf024b7ec0e745669ed392e
Dhiman Sarkar <dhiman.eminem@gmail.com>
2026-03-15 15:05:46 +0530

    config update

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,9 +4,12 @@ SITEURL = ""
 
 PATH = "content"
 
+# THEME = 'themes/mytheme'
+THEME_STATIC_DIR = 'assets'    
+
 TIMEZONE = 'Asia/Kolkata'
 
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -41,3 +44,19 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+
+
+# ────────────────────────────────────────────────────────────
+#  URL STRUCTURE
+# ────────────────────────────────────────────────────────────
+ARTICLE_URL        = '{category}/{slug}/'
+ARTICLE_SAVE_AS    = '{category}/{slug}/index.html'
+ 
+PAGE_URL           = '{slug}/'
+PAGE_SAVE_AS       = '{slug}/index.html'
+ 
+CATEGORY_URL       = 'category/{slug}/'
+CATEGORY_SAVE_AS   = 'category/{slug}/index.html'
+ 
+TAG_URL            = 'tag/{slug}/'
+TAG_SAVE_AS        = 'tag/{slug}/index.html'
